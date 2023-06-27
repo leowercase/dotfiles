9ea866d5555c273459f81606e668dd6fdfb64744
leowercase <126769796+leowercase@users.noreply.github.com>
2023-06-27 22:09:16 +0000

    Added *ultimate wisdom* Now it should actually work as expected lol

--- modules/admin.nix
+++ modules/admin.nix
@@ -11,7 +11,14 @@
         "The adminstrator user of the system, who owns the configuration directory at `/etc/nixos`";
     };
 
-    config.system.activationScripts.chown_config_to_admin.text = ''
-      chown -R ${cfg} /etc/nixos
-    '';
+    config.system.activationScripts.chown_config_to_admin = {
+      deps = [ "users" ];
+      text =
+        # Getting the `name` from `users.users` verifies that the
+        # user is actually defined
+        let inherit (config.users.users.${cfg}) name;
+        in ''
+          chown -R ${name}: /etc/nixos
+        '';
+    };
   }
