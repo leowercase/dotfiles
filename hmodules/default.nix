36e4fdbdd254d36280142eec9d04d68ab3bee349
leowercase <126769796+leowercase@users.noreply.github.com>
2023-06-28 23:06:54 +0000

    Made user directories default to lowercase names eg. `~/downloads` vs. `~/Downloads`

--- hmodules/default.nix
+++ hmodules/default.nix
@@ -1,4 +1,4 @@
-{ config, username, myLib, ... }:
+{ config, lib, username, myLib, ... }:
   {
     imports = myLib.getModules ./.;
 
@@ -16,4 +16,27 @@
 
     # Symlink $HOME/.config/home-manager to the flake
     xdg.configFile."home-manager".source = config.lib.file.mkOutOfStoreSymlink /etc/nixos;
+
+    xdg = {
+      enable = true;
+      # Lowercase user directories by default
+      userDirs =
+        {
+          enable = true;
+          createDirectories = true;
+        }
+        //
+	(builtins.mapAttrs
+          (_: dir: lib.mkDefault "${config.home.homeDirectory}/${dir}")
+	  {
+            desktop = "desktop";
+            documents = "documents";
+            download = "downloads";
+            music = "music";
+            pictures = "pictures";
+            publicShare = "public";
+            templates = "templates";
+            videos = "videos";
+          });
+    };
   }
