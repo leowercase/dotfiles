483c283f29be411b1b38e24957220c534f459924
leowercase <126769796+leowercase@users.noreply.github.com>
2023-06-29 22:21:48 +0000

    Added the gaming suite GAMING

---
+++ hmodules/users/leo/suites/gaming.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+  with lib;
+  let
+    cfg = config.leo.suites.gaming;
+  in
+  {
+    options.leo.suites.gaming = {
+      enable = mkEnableOption "the gaming suite";
+    };
+
+    config = mkIf cfg.enable {
+      home.packages = with pkgs; [
+        steam gamescope
+      ];
+    };
+  }
