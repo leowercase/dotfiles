4c799e05c7d846ed65645056352425267e70581f
leowercase <126769796+leowercase@users.noreply.github.com>
2023-06-29 19:14:25 +0000

    Edited Neovim module Added Neovide, nvimpager and config directory support.

--- hmodules/users/leo/programs/neovim/default.nix
+++ hmodules/users/leo/programs/neovim/default.nix
@@ -6,9 +6,19 @@
   {
     options.leo.programs.neovim = {
       enable = mkEnableOption "my Neovim";
+      pager = mkEnableOption "nvimpager" // { default = cfg.enable; };
+      gui = mkEnableOption "Neovide" // { default = cfg.enable; };
     };
 
     config = mkIf cfg.enable {
-      home.packages = [ pkgs.neovim ];
+      home.packages = with pkgs; [ neovim ]
+        ++ (if cfg.pager then [ nvimpager ] else [])
+	++ (if cfg.gui then [ neovide ] else []);
+
+      xdg.configFile."nvim" = {
+        source = ./.;
+	recursive = true;
+      };
+      xdg.configFile."nvim/default.nix".enable = false;
     };
   }
