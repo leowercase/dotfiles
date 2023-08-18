9fa681fd869da853fa06e328b51d97405fb1a821
leowercase <126769796+leowercase@users.noreply.github.com>
2023-08-18 14:12:10 +0300

    Add more search engines to qutebrowser

--- users/leo/programs/qutebrowser.nix
+++ users/leo/programs/qutebrowser.nix
@@ -38,14 +38,21 @@
         };
         
         searchEngines = {
+	  # Search engines
           DEFAULT = "https://search.brave.com/search?q={}";
 	  "!img" = "https://search.brave.com/images?q={}";
+
+          # Wikis
+          "!w" = "https://en.wikipedia.org/w/index.php?title=Special:Search&search={}";
 
           # Distro wikis
           "!aw" = "https://wiki.archlinux.org/index.php?search={}";
           "!now" = "https://nixos.wiki/index.php?search={}";
           "!gw" = "https://wiki.gentoo.org/index.php?search={}";
           "!vhb" = "https://docs.voidlinux.org/?search={}";
+
+	  # Dictionaries
+	  "!ubd" = "https://www.urbandictionary.com/define.php?term={}";
 
           # NixOS search engines
           "!pkgs" = "https://search.nixos.org/packages?query={}";
