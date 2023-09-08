753b67e81392d143a8e8a7ffb8bc983a58c8ad94
leowercase <126769796+leowercase@users.noreply.github.com>
2023-09-08 09:22:41 +0300

    Add more things to Neovim configuration

--- users/leo/programs/neovim/plugins.nix
+++ users/leo/programs/neovim/plugins.nix
@@ -1,3 +1,26 @@
 _: {
+  plugins.lsp = {
+    enable = true;
+    servers.rnix-lsp.enable = true;
+  };
+
+  plugins.treesitter = {
+    enable = true;
+    nixGrammars = true;
+    ensureInstalled = "all";
+    nixvimInjections = true;
+    moduleConfig.highlight.enable = true;
+  };
+  plugins.treesitter-context.enable = true;
+
+  plugins.nix.enable = true;
+
+  plugins.indent-blankline = {
+    enable = true;
+    useTreesitter = true;
+    showCurrentContext = true;
+    showCurrentContextStart = true;
+  };
+
   plugins.lualine.enable = true;
 }
