06b4acbc73a70e26909d580373424743b27862b0
leowercase <126769796+leowercase@users.noreply.github.com>
2023-11-08 19:09:12 +0200

    Add Godot

--- users/leo/suites/arts.nix
+++ users/leo/suites/arts.nix
@@ -1,11 +1,12 @@
 { lib, pkgs, ... }:
   with lib;
   custom.mkSuite "arts" [ "users'" "leo" "suites" "arts" ] {
-    parts = [ "krita" "lmms" ];
+    parts = [ "krita" "lmms" "godot" ];
 
     config = cfg: {
       hm.leo.home.packages = with pkgs;
         (optional (cfg.krita) krita)
-        ++ (optional (cfg.lmms) lmms);
+        ++ (optional (cfg.lmms) lmms)
+        ++ (optional (cfg.godot) godot_4);
     };
   }
