c61bfa3878620c90e9503126b1e33f71f836646d
leowercase <126769796+leowercase@users.noreply.github.com>
2023-11-12 13:58:52 +0200

    Change the default tab size of Neovim

--- users/leo/programs/neovim/options.nix
+++ users/leo/programs/neovim/options.nix
@@ -1,12 +1,18 @@
 _: {
   programs.nixvim = {
     luaLoader.enable = true;
+
     clipboard.register = "unnamedplus";
+
     options = {
       number = true;
       relativenumber = true;
       
       termguicolors = true;
+
+      # Tabs are 4 spaces long by default
+      shiftwidth = 4;
+      tabstop = 4;
     };
   };
 }
