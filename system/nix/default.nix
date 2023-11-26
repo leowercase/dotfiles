e1a63f0597fe3e2974947469dbfadded24cb6650
leowercase <126769796+leowercase@users.noreply.github.com>
2023-11-26 12:42:36 +0200

    Add custom entries to the system registry

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -10,6 +10,26 @@
       overlays = [ (import ./overlay.nix) ];
     };
 
+    # Custom Nix system registry entries. This allows me to, for example,
+    # run `nix run dotfiles#nvim` instead of `nix run github:leowercase/dotfiles#nvim`
+    nix.registry = {
+      nixpkgs = {
+        from = {
+          id = "nixpkgs";
+          type = "indirect";
+        };
+        flake = flake.nixpkgs;
+      };
+
+      dotfiles = {
+        from = {
+          id = "dotfiles";
+          type = "indirect";
+        };
+        flake = flake.self;
+      };
+    };
+
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
