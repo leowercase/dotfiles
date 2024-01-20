963df177f96ae9060f1ef437ae17752cd348bbbd
leowercase <126769796+leowercase@users.noreply.github.com>
2024-01-20 13:47:32 +0200

    Nitpick hyprland

--- users/leo/desktop/hyprland/default.nix
+++ users/leo/desktop/hyprland/default.nix
@@ -10,63 +10,61 @@
       enable = mkEnableOption "my Hyprland";
     };
 
-    config = mkMerge [
-      { home-manager.users.leo.imports = [ inputs.hyprland.homeManagerModules.default ]; }
+    config = mkIf cfg.enable {
+      home-manager.users.leo.imports = [ inputs.hyprland.homeManagerModules.default ];
 
-      (mkIf cfg.enable {
-        programs.hyprland.enable = true;
-        system'.audio = "pipewire";
+      programs.hyprland.enable = true;
+      system'.audio = "pipewire";
 
-        home-manager.users.leo.wayland.windowManager.hyprland = {
-          enable = true;
+      home-manager.users.leo.wayland.windowManager.hyprland = {
+        enable = true;
 
-          plugins = [ inputs'.hyprland-plugins.packages.hyprbars ];
+        plugins = [ inputs'.hyprland-plugins.packages.hyprbars ];
 
-          settings = mkMerge [
-            (import ./settings.nix args)
-            (import ./binds.nix args)
-          ];
-
-          extraConfig = import ./config.nix args;
-        };
-
-        home-manager.users.leo.home.packages = with pkgs; [
-          wl-clipboard
-          libsForQt5.polkit-kde-agent
-
-          rofi-wayland
-          inputs'.hyprland-contrib.packages.grimblast
-
-          noto-fonts
-          noto-fonts-emoji
-          noto-fonts-cjk-sans
-          noto-fonts-cjk-serif
-          (nerdfonts.override { fonts = [ "FiraCode" ]; })
-
-          # See https://wiki.archlinux.org/title/Wayland#GUI_libraries
-          libsForQt5.qt5.qtwayland
-          qt6.qtwayland
+        settings = mkMerge [
+          (import ./settings.nix args)
+          (import ./binds.nix args)
         ];
-      
-        users'.leo.fonts.aliases = {
-          serif = "Noto Serif";
-          sans-serif = "Noto Sans";
-          monospace = "FiraCode Nerd Font";
-          emoji = "Noto Color Emoji";
-        };
 
-        users'.leo.programs = {
-          swaylock-effects.enable = true;
+        extraConfig = import ./config.nix args;
+      };
 
-          swayidle = {
-            enable = true;
-            screenLocker = "${pkgs.swaylock-effects}/bin/swaylock";
-          };
-        };
+      home-manager.users.leo.home.packages = with pkgs; [
+        wl-clipboard
+        libsForQt5.polkit-kde-agent
 
-        home-manager.users.leo.services.dunst = {
+        rofi-wayland
+        inputs'.hyprland-contrib.packages.grimblast
+
+        noto-fonts
+        noto-fonts-emoji
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        (nerdfonts.override { fonts = [ "FiraCode" ]; })
+
+        # See https://wiki.archlinux.org/title/Wayland#GUI_libraries
+        libsForQt5.qt5.qtwayland
+        qt6.qtwayland
+      ];
+    
+      users'.leo.fonts.aliases = {
+        serif = "Noto Serif";
+        sans-serif = "Noto Sans";
+        monospace = "FiraCode Nerd Font";
+        emoji = "Noto Color Emoji";
+      };
+
+      users'.leo.programs = {
+        swaylock-effects.enable = true;
+
+        swayidle = {
           enable = true;
+          screenLocker = "${pkgs.swaylock-effects}/bin/swaylock";
         };
-      })
-    ];
+      };
+
+      home-manager.users.leo.services.dunst = {
+        enable = true;
+      };
+    };
   }
