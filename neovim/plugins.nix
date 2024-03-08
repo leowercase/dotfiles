95940d2351fd4c5d04e1459eb41cc7ad644ac051
leowercase <126769796+leowercase@users.noreply.github.com>
2024-03-08 16:19:32 +0200

    Fix NixVim

--- neovim/plugins.nix
+++ neovim/plugins.nix
@@ -17,32 +17,28 @@ _: {
     nix.enable = true;
 
     luasnip.enable = true;
-    nvim-cmp = {
+    cmp = {
       enable = true;
-      snippet.expand = "luasnip";
-      mapping = {
-        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-f>" = "cmp.mapping.scroll_docs(4)";
-        "<Tab>" = {
-          modes = [ "i" "s" ];
-          action = "cmp.mapping.select_next_item()";
+      settings = {
+        snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+        mapping = {
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' })";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })";
         };
-        "<S-Tab>" = {
-          modes = [ "i" "s" ];
-          action = "cmp.mapping.select_prev_item()";
-        };
-      };
 
-      sources = [
-        { name = "path"; }
-        { name = "nvim_lsp"; }
-        { name = "luasnip"; }
-        {
-          name = "buffer";
-          # Words from other open buffers can also be suggested
-          option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
-        }
-      ];
+        sources = [
+          { name = "path"; }
+          { name = "nvim_lsp"; }
+          { name = "luasnip"; }
+          {
+            name = "buffer";
+            # Words from other open buffers can also be suggested
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          }
+        ];
+      };
     };
 
     fidget.enable = true;
